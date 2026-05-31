bbf711aeffb3e43aa3ce633ec8fde42442890e84
asbdu463bca22825 <148364701+asbdu463bca22825@users.noreply.github.com>
2026-05-31 19:29:15 +0530

    Update Task5_ covert_oops.php

--- Task5_ covert_oops.php
+++ Task5_ covert_oops.php
@@ -1,3 +1,7 @@
+
+
+##covert the connection in oops
+
 <?php
 
 class Database
@@ -18,3 +22,77 @@ class Database
 $db = new Database();
 
 ?>
+
+
+
+###convert the registration in oops
+
+
+<?php
+
+class UserRegistration
+{
+    private $conn;
+
+    // Constructor
+    public function __construct()
+    {
+        $this->conn = mysqli_connect(
+            "localhost",
+            "root",
+            "",
+            "student_db"
+        );
+
+        if (!$this->conn) {
+            die("Connection Failed");
+        }
+    }
+
+    // Register User Method
+    public function registerUser($name, $email, $password, $confirm_password, $phone_number)
+    {
+        $sql = "INSERT INTO users(name, email, password, confirm_password, phone_number)
+                VALUES('$name', '$email', '$password', '$confirm_password', '$phone_number')";
+
+        if (mysqli_query($this->conn, $sql)) {
+            echo "Registration Successful";
+        } else {
+            echo "Error";
+        }
+    }
+
+    // Close Connection
+    public function closeConnection()
+    {
+        mysqli_close($this->conn);
+    }
+}
+
+// Create Object
+$user = new UserRegistration();
+
+// Get Form Data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+$phone_number = $_POST['phone_number'];
+
+// Call Method
+$user->registerUser(
+    $name,
+    $email,
+    $password,
+    $confirm_password,
+    $phone_number
+);
+
+// Close Connection
+$user->closeConnection();
+
+?>
+
+
+
+##convert login in oops
