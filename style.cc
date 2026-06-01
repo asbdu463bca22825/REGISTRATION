0e68206870a61973b03b589bd42c424e76deb91e
asbdu463bca22825 <148364701+asbdu463bca22825@users.noreply.github.com>
2026-06-01 21:22:06 +0530

    Create style.cc with initial CSS styles
    Add basic styles for body, form, input, button, and h1 elements.

---
+++ style.cc
@@ -0,0 +1,33 @@
+body{
+
+    font-family: Arial;
+    background-color: rgb(240, 248, 240);
+}
+form{
+    width:300px;
+    margin:100px auto;
+    padding:20px;
+    background-color: rgb(47, 224, 255);
+    border-radius: 10px;
+}
+input{
+    width:90%;
+    padding: 10px;
+    margin:8px auto;
+}
+button{
+    width:100%;
+    padding:10px;
+    background-color: whit;
+    color: aqua;
+    border:non;
+
+
+}
+h1{
+    text-align: center;
+    background-color: rgb(242, 245, 237);
+ 
+    
+
+}
